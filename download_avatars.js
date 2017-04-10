e3efc6b7ef6e6285223ea557231c010b51f30867
Jacques Begin <jacques.andre.begin@gmail.com>
2017-04-10 22:23:49 +0000

    Creates request URL string from parameter data

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,13 @@ var request = require("request");
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
-function getRepoContributors(repoOwner, repoName, cb) {
 
+var GITHUB_USER = "HombreLoco";
+var GITHUB_TOKEN = "9dce65ff12e3df64c728a9b476037e0aa081e90b";
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`
+  console.log(requestURL);
 }
 
 
