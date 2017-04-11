de5f0677fac0d70051d0f199d3a363ae90e8b9b2
Jacques Begin <jacques.andre.begin@gmail.com>
2017-04-11 01:58:21 +0000

    Downloading contributor images

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require("request");
+var fs = require("fs");
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
@@ -7,7 +8,6 @@ var GITHUB_USER = "HombreLoco";
 var GITHUB_TOKEN = "9dce65ff12e3df64c728a9b476037e0aa081e90b";
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // var requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`
   var options = {
     url: `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
@@ -24,19 +24,17 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-// function getUserAvatars(err, result) {
-//   var avatars = [];
-//   for (var i in result) {
-//     avatars.push(result[i].avatar_url);
-//   }
-//   console.log(avatars);
-// }
-
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+  .on("error", function (err) {
+          throw err;
+  })
+  .pipe(fs.createWriteStream(filePath));
+}
 
 getRepoContributors("jQuery", "jQuery", function(err, result) {
   var avatars = [];
   for (var i in result) {
-    avatars.push(result[i].avatar_url);
+    downloadImageByURL(result[i].avatar_url, `./avatars/${result[i].login}.jpg`)
   }
-  console.log(avatars);
 });
